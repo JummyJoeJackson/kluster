056a50e1a96c081b8918b782a48695e5d366f011
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2025-10-23 00:47:20 -0400

    Remove unused import 'redirect' from profile.py

--- kluster/routes/profile.py
+++ kluster/routes/profile.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from kluster.models import User, db
 import structlog
